d6868cde3d027fbd69f876f5f9dd27ff25b9c5de
teg <sporsmaal2@yahoo.co.uk>
2016-01-09 20:43:59 +0100

    morestuff

--- Assets/teg/Scripts/TrackEditor.cs
+++ Assets/teg/Scripts/TrackEditor.cs
@@ -1,13 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Text;
 
 //this script places the next track bit at the end point of the last track bit
 //and swaps between bits (prefabs)
 
 public class TrackEditor : MonoBehaviour {
 
+    public bool bEditor = true;
+
     public SaveLoad SaveGame;
+    FileInfo f;
 
     public Camera Cam;
     float CamHeight;
@@ -36,19 +41,28 @@ public class TrackEditor : MonoBehaviour {
             CurrentTransform = lastBit.transform.Find("end").transform;
         }
 
-        if (nextBit == null)
+        if (bEditor)
         {
-            //Instantiate(Resources.Load("enemy"));
-            nextBit = (GameObject)Instantiate(AvailableBits[Index], CurrentTransform.position, CurrentTransform.rotation);
-        }
+            if (nextBit == null)
+            {
+                nextBit = (GameObject)Instantiate(AvailableBits[Index], CurrentTransform.position, CurrentTransform.rotation);
+            }
 
-        if (Cam != null)
-        {
-            CamHeight = Cam.transform.position.y;
-            Vector3 newPosition = CurrentTransform.position;
-            newPosition.y = CamHeight;
-            CamLoc = newPosition;
+            if (Cam != null)
+            {
+                CamHeight = Cam.transform.position.y;
+                Vector3 newPosition = CurrentTransform.position;
+                newPosition.y = CamHeight;
+                CamLoc = newPosition;
+            }
+
         }
+        else
+        {
+            //load from file
+            LoadTrack();
+        }
+   
 	}
 
     public void ChangeBit()
@@ -73,14 +87,17 @@ public class TrackEditor : MonoBehaviour {
         if (nextBit != null)
         {
             Destroy(nextBit);
+        }
+            nextBit = (GameObject)Instantiate(AvailableBits[Index], CurrentTransform.position, CurrentTransform.rotation);
+
             Index++;
             if (Index >= AvailableBits.Length)
             {
                 Index = 0;
             }
 
-            nextBit = (GameObject)Instantiate(AvailableBits[Index], CurrentTransform.position, CurrentTransform.rotation);
-        }
+            
+        
 
         CurrentTransform = nextBit.transform.Find("end").transform;
         lastBit = nextBit;
@@ -118,10 +135,81 @@ public class TrackEditor : MonoBehaviour {
 
     public void SaveTrack()
     {
+        StreamWriter w;
+
+        f = new FileInfo(Application.persistentDataPath + "\\" + "myFile.txt");
+
+        //create new file, delete old if it exists
+        if (!f.Exists)
+        {
+            w = f.CreateText();
+            print("creating file");
+        }
+        else
+        {
+            f.Delete();
+            w = f.CreateText();
+            print("saving file");
+        }
+        
         //add current bit
-        TrackBits.Add(Index);
-        //save
-        SaveGame.SaveTrack(TrackBits, TrackBits.Count);
+       // TrackBits.Add(Index);
+
+        print("savetrack() length: "+ TrackBits.Count);
+        //write trackbits array, 1 element on each line
+        for (int i = 0; i < TrackBits.Count; i++)
+        {
+            string str = TrackBits[i].ToString();
+            w.WriteLine(str);
+            print("savetrack() value: " + str);
+        }
+        w.Close();
+            
+    }
+
+    public void LoadTrack()
+    {
+        // Handle any problems that might arise when reading the text
+        
+            string line;
+
+            string DataPath = Application.persistentDataPath + "\\" + "myFile.txt";
+            StreamReader theReader = new StreamReader(DataPath, Encoding.Default);
+
+        while ((line = theReader.ReadLine()) != null)
+        {
+            int value;
+
+            // attempt to parse the value using the TryParse functionality of the integer type
+            int.TryParse(line, out value);
+            print("loadtrack() line, value: " + line + ", " +(int) value);
+            LoadBit(value);
+        }
+
+
+        Destroy(nextBit);
+
+
+
+
+        /*  string[] Readfile = new string[File.ReadAllLines(DataPath).Length];
+
+          print("loadtrack() length: "+ Readfile.Length);
+          print("loadtrack() whats in it? 0: " + Readfile[0]);
+
+          for (int i = 0; i < Readfile.Length; i++)
+          {
+              //value is the index of the track bit, got from a line in a text file
+              int value;
+
+              // attempt to parse the value using the TryParse functionality of the integer type
+              int.TryParse(Readfile[i], out value);
+              print("loadtrack() i, value: " + i+ ", "+ Readfile[i]);
+
+              //spawn bit
+              LoadBit(value);
+          }*/
+
     }
 	
 	// Update is called once per frame
