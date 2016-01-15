ad4bbe9ce2225812b4c82e21f36174178163bff0
teg <sporsmaal2@yahoo.co.uk>
2016-01-15 13:22:29 +0100

    poj

---
+++ Assets/teg/Scripts/LapTimer.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LapTimer : MonoBehaviour {
+
+    private float startTime;
+    private float ellapsedTime;
+    private float lastLap;
+    bool Started = false;
+
+ 
+
+    // Use this for initialization
+    void Start () {
+        startTime = Time.time;
+    
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Started)
+        {
+            ellapsedTime = Time.time - startTime;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        print("trigger = "+ other.tag);
+        if (other.gameObject.tag == "Player")
+        {
+            print("trigger player = true");
+            
+
+            if (Started)
+            {
+                lastLap = ellapsedTime;
+                startTime = Time.time;
+            }
+            Started = true;
+
+        }
+        
+    }
+
+    void OnGUI()
+    {
+        GUI.contentColor = Color.grey;
+        GUI.Label(new Rect(100, 30, 100, 20), (lastLap.ToString()));
+        GUI.contentColor = Color.black;
+        GUI.Label(new Rect(100, 50, 100, 20), (ellapsedTime.ToString()));
+    }
+}
